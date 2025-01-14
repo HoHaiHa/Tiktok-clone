4f7ff35bd6262a9a688da6db8249a003a85b4e94
hohaiha <hohaiha0210@gmail.com>
2025-01-14 12:22:21 +0700

    continue build layout

--- src/component/Layout/DefaultLayout/index.js
+++ src/component/Layout/DefaultLayout/index.js
@@ -1,5 +1,9 @@
 import Header from "~/component/Layout/Components/Header";
 import Sidebar from "./Sidebar";
+import styles from "./DefaltLayout.module.scss"
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({children}){
     return (
