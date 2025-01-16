fd1a0f4798cabaeccc6bbd3cb176704cce58c3e8
hohaiha <hohaiha0210@gmail.com>
2025-01-16 16:29:45 +0700

    continue build layout

--- src/component/Layout/Components/Header/index.js
+++ src/component/Layout/Components/Header/index.js
@@ -4,7 +4,9 @@ import styles from './Header.module.scss'
 const cx = classNames.bind(styles)
 
 function Header(){
-    return <header className={cx('wrapper')}></header>
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}></div>
+    </header>
 }
 
 export default Header;
--- src/component/Layout/DefaultLayout/index.js
+++ src/component/Layout/DefaultLayout/index.js
@@ -7,13 +7,11 @@ const cx = classNames.bind(styles);
 
 function DefaultLayout({children}){
     return (
-        <div>
-            <Header/>
-            <div className="container">
+        <div className={cx('wrapper')}>
+            <Header></Header>
+            <div className={cx('container')}>
                 <Sidebar/>
-                <div className="content">
-                    {children}
-                </div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     )
